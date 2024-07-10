08667933fd6b4c40ac7688b6e64666c7596619bd
Akshaan Bandara <53240589+AkshaanB@users.noreply.github.com>
2024-07-10 23:40:16 +0530

    Update cnn,py

--- nns/cnn.py
+++ nns/cnn.py
@@ -12,18 +12,44 @@ class CNNLayer():
 
         if self.padding:
             padded_input_mtx = F.pad(input_mtx, self.padding, mode='constant', value=0)
-            conv_output = 
+
+            # Equation to get output size = (input_size - kernel_size + 2 * padding) / stride + 1
+            output_mtx_height = input_mtx.shape[0]
+            output_mtx_width = input_mtx.shape[1]
+
+            kernel_height, kernel_width = kernel.shape
+
+            output_mtx = torch.zeros((output_mtx_height, output_mtx_width), dtype=torch.int)
+
+            for i in range(output_mtx_height):
+                for j in range(output_mtx_width):
+                    region = padded_input_mtx[i:i+kernel_height, j:j+kernel_width]
+                    output_mtx[i,j] = torch.sum(region * kernel)
+
+            conv_output = output_mtx
         else:
-            conv_output = 
+            # Equation to get output size = (input_size - kernel_size + 2 * padding) / stride + 1
+            output_mtx_height = input_mtx.shape[0] - kernel.shape[0] + 1
+            output_mtx_width = input_mtx.shape[1] - kernel.shape[1] + 1
+
+            kernel_height, kernel_width = kernel.shape
+
+            output_mtx = torch.zeros((output_mtx_height, output_mtx_width), dtype=torch.int)
+
+            for i in range(output_mtx_height):
+                for j in range(output_mtx_width):
+                    region = input_mtx[i:i+kernel_height, j:j+kernel_width]
+                    output_mtx[i,j] = torch.sum(region * kernel)
+
+            conv_output = output_mtx
 
         output = torch.relu(conv_output)
 
         return output
 
 
-
-
 if __name__ == "__main__":
+    # cnnlayer = CNNLayer(1, padding=None)
     cnnlayer = CNNLayer(1, padding=(1, 1, 1, 1))
     input_mtx = torch.tensor([[-45, 12, 5, 17],
                 [22, 10, -35, 6],
